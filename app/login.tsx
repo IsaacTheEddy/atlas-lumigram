41196c5383b92a81744c66603ea78ac2ad363b0d
Dtbissy <isaactheedwards@gmail.com>
2025-06-26 16:43:27 -0500

    Messing around with firebase

--- app/login.tsx
+++ app/login.tsx
@@ -1,13 +1,40 @@
-import { View, Text, Pressable } from "react-native";
+import { View, Text, Pressable, Alert } from "react-native";
 import { Link, useRouter } from "expo-router";
 import { StyleSheet } from "react-native";
 import { TextInput } from "react-native";
 import { useState } from "react";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 export default function Page() {
   const router = useRouter();
   const [email, onChangeEmail] = useState("");
   const [password, onChangePassword] = useState("");
+
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyB57_XPj6XFTkv1hbB1G2XpeQRuOYP_irY",
+    authDomain: "lumigram-9d2ae.firebaseapp.com",
+    projectId: "lumigram-9d2ae",
+    storageBucket: "lumigram-9d2ae.firebasestorage.app",
+    messagingSenderId: "1008387069763",
+    appId: "1:1008387069763:web:f47ea40378746594d301f6",
+    measurementId: "G-85G6MJ395L",
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+
   return (
     <View style={styles.pageView}>
       {/* Atlas School */}
@@ -81,9 +108,16 @@ export default function Page() {
             borderRadius: 6,
             justifyContent: "center",
           }}
-          onPress={() => {
-            router.push("./(tabs)/");
-          }}
+          onPress={() =>
+            signInWithEmailAndPassword(auth, email, password)
+              .then((userCredential) => {
+                const user = userCredential.user;
+                router.push("./(tabs)/");
+              })
+              .catch((error) => {
+                const err = console.error(error);
+              })
+          }
         >
           <Text style={{ flex: 0, textAlign: "center", color: "white" }}>
             Sign In
---
+++ firebaseConfig.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB57_XPj6XFTkv1hbB1G2XpeQRuOYP_irY",
+    authDomain: "lumigram-9d2ae.firebaseapp.com",
+    projectId: "lumigram-9d2ae",
+    storageBucket: "lumigram-9d2ae.firebasestorage.app",
+    messagingSenderId: "1008387069763",
+    appId: "1:1008387069763:web:f47ea40378746594d301f6",
+    measurementId: "G-85G6MJ395L"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
